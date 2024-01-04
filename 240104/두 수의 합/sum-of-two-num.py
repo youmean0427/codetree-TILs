23d71d5547104860cce723bccaa9b68ff91ae07c
YM <109326287+youmean0427@users.noreply.github.com>
2024-01-04 18:52:33 +0900

    Title: 두 수의 합, Time: 192ms, Memory: 39MB, Status: Passed - CodeTree

--- 240104/두 수의 합/sum-of-two-num.py
+++ 240104/두 수의 합/sum-of-two-num.py
@@ -1,15 +1,12 @@
-from itertools import combinations
-
+from collections import defaultdict
 n, k = map(int, input().split())
-arr = {}
-c = 0
-for i in input().split():
-    arr[c] = int(i)
-    c += 1
+arr = [int(i) for i in input().split()]
 
-cnt = 0
-for i in range(n):
-    for j in range(i+1, n):
-        if arr[i] + arr[j] == k:
-            cnt += 1
-print(cnt)
+cnt = defaultdict(int)
+ans = 0
+for i in arr:
+    t = k - i
+    ans += cnt[t] 
+    cnt[i] += 1    
+    
+print(ans)
