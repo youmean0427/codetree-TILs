0bc8675f761e97d1dbbd6efb6363e73b1c61063e
YM <109326287+youmean0427@users.noreply.github.com>
2024-01-04 18:08:55 +0900

    Title: 가장 많은 데이터, Time: 423ms, Memory: 35MB, Status: Passed - CodeTree

--- 240104/가장 많은 데이터/most-data.py
+++ 240104/가장 많은 데이터/most-data.py
@@ -2,7 +2,10 @@ from collections import defaultdict
 
 n = int(input())
 cnt = defaultdict(int)
+ans = 0
 for _ in range(n):
-    cnt[input()] += 1
-sorted_cnt = sorted(cnt.items(), key=lambda x: -x[1])
-print(sorted_cnt[0][1])
+    k = input()
+    cnt[k] += 1
+    ans = max(ans, cnt[k])
+
+print(ans)
