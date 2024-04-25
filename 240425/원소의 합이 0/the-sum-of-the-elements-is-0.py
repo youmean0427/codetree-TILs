6c2211ed7a920380a9b45d6ffafd2ffb9d0625b7
YM <109326287+youmean0427@users.noreply.github.com>
2024-04-25 19:55:32 +0900

    Title: 원소의 합이 0, Time: 3404ms, Memory: 25MB, Status: Passed - CodeTree

--- 240425/원소의 합이 0/the-sum-of-the-elements-is-0.py
+++ 240425/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -1,20 +1,19 @@
+from collections import defaultdict
 n = int(input())
 nums = {}
 for i in range(4):
     nums[i] = list(map(int, input().split()))
 ans = 0
-count = {}
+count = defaultdict(int)
+for i in nums[3]:
+    count[i] += 1
+
 for i in nums[0]:
     for j in nums[1]:
         for k in nums[2]:
             diff = -(i + j + k)
 
-            if diff in nums[3]:
-                ans += 1
-    
-    if i in count:
-        count[i] += 1
-    else:
-        count[i] = 1
+            if diff in count:
+                ans += count[diff]
 
 print(ans)
