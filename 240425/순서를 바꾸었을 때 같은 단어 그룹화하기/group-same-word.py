c457d02f01335a83a8c6dae2f918bb120c36f517
YM <109326287+youmean0427@users.noreply.github.com>
2024-04-25 20:40:18 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 340ms, Memory: 26MB, Status: Passed - CodeTree

--- 240425/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
+++ 240425/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
@@ -3,23 +3,10 @@ from collections import defaultdict
 N = int(input())
 total = defaultdict(int)
 ans = 0
-arr = []
 for i in range(N):
     S = input()
-    cnt = {}
-    for s in S:
-        if s in cnt:
-            cnt[s] += 1
-        else:
-            cnt[s] = 1
-    
-    arr.append(cnt)
+    S = sorted(S)
+    total["".join(S)] += 1
 
-ans = 0
-for i in range(N):
-    cnt = 0
-    for j in range(i, N):
-        if arr[i] == arr[j]:
-            cnt += 1
-    ans = max(ans, cnt)
-print(ans)
+x = sorted(total.values(), reverse=True)
+print(x[0])
