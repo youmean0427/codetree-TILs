61b92019e4534032eb9311c8d48e2137ad8a2a16
YM <109326287+youmean0427@users.noreply.github.com>
2024-04-26 18:41:24 +0900

    Title: 초대장과 번호표, Time: 5010ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

--- 240426/초대장과 번호표/invitation-and-number-tag.py
+++ 240426/초대장과 번호표/invitation-and-number-tag.py
@@ -1,23 +1,28 @@
 N, G = map(int, input().split())
 invite = set()
+y = []
 for _ in range(G):
     x = list(map(int, input().split()))
-    cnt = 0
-    temp = set()
-    for i in range(1, len(x)):
-        if cnt == len(x)-2:
-            invite.add(x[i])
-            break
-        if x[i] == 1:
-            invite.add(x[i])
-            cnt += 1
-        elif x[i] in invite:
-            cnt += 1
-        else:
-            temp.add(x[i])
+    y.append(x)
 
-    if cnt == len(x)-2:
-        for t in temp:
-            invite.add(t)
+for c in range(G):
+    for i in range(G):
+        x = y[i]
+        gp = x[0]
+
+        cnt = 0
+        temp = []
+        for i in x[1:]:
+            if i == 1:
+                invite.add(i)
+                cnt += 1
+            elif i in invite:
+                cnt += 1
+            else:
+                temp.append(i)
         
+        if cnt == gp - 1:
+            invite.add(temp[0])
+
+        # print(x, invite, temp)        
 print(len(invite))
