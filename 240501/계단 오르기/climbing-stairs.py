b2d2c533f5e1141bf1cc98ceedeb7b29245dc2d4
YM <109326287+youmean0427@users.noreply.github.com>
2024-05-01 23:37:57 +0900

    Title: 계단 오르기, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240501/계단 오르기/climbing-stairs.py
@@ -0,0 +1,10 @@
+n = int(input())
+dp = [0 for _ in range(n+1)]
+dp[1] = 0
+dp[2] = 1
+if n >= 3:
+    dp[3] = 1
+    
+for i in range(4, n+1):
+    dp[i] = (dp[i-2] + dp[i-3]) % 10007
+print(dp[n])
