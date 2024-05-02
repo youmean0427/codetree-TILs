f105442a74f3076af99b526ce61280f978af3ae8
YM <109326287+youmean0427@users.noreply.github.com>
2024-05-02 17:50:06 +0900

    Title: 배열 추출, Time: 488ms, Memory: 34MB, Status: Passed - CodeTree

---
+++ 240502/배열 추출/array-extraction.py
@@ -0,0 +1,12 @@
+import heapq
+n = int(input())
+pq = []
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        if pq:
+            print(-heapq.heappop(pq))
+        else:
+            print(0)
+    else:
+        heapq.heappush(pq, -x)
