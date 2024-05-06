9e0cadbcd82c4e67155ad10d5a248f28b1b4f3b1
YM <109326287+youmean0427@users.noreply.github.com>
2024-05-06 23:11:32 +0900

    Title: 행복한 수열의 개수, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240506/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240506/행복한 수열의 개수/number-of-happy-sequence.py
@@ -4,22 +4,18 @@ for _ in range(n):
     arr.append(list(map(int, input().split())))
 ans = 0
 
-def find(arr, m):
+def lucky (arr, m):
     global ans
-    for a in arr:
-        x = a[0]
-        max_cnt = 0
-        cnt = 1
-        for i in range(1, len(a)):
-            if a[i] == x:
-                cnt += 1
-            else:
-                x = a[i]
-                max_cnt = max(max_cnt, cnt)
-        max_cnt = max(max_cnt, cnt)
-        if max_cnt >= m:
+    for i in range(len(arr)-m+1):
+        x = arr[i:i+m]
+        if x.count(x[0]) == m:
             ans += 1
+            return
 
+def find(arr, m):
+    for a in arr:
+        lucky(a, m)
+        
 find(arr, m)
 
 col_arr = []
