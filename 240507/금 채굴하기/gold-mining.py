835dba10120f5665d9a6425ae16c193390760a42
YM <109326287+youmean0427@users.noreply.github.com>
2024-05-07 17:33:49 +0900

    Title: 금 채굴하기, Time: 124ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240507/금 채굴하기/gold-mining.py
+++ 240507/금 채굴하기/gold-mining.py
@@ -1,22 +1,14 @@
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-k_arr = [1]
-m_count = 3
-while True:
-    if m_count > N:
-        break
-    k_arr.append(m_count)
-    m_count += 2
-
 bfs_count = [0, 1]
 bc = 4
-
 while True:
-    if len(bfs_count) == len(k_arr):
+    if len(bfs_count) == 10:
         break
     bfs_count.append(bfs_count[-1] + bc)
     bc += 4
+
 
 def bfs(n, m, bfs_c):
 
@@ -49,12 +41,12 @@ def bfs(n, m, bfs_c):
                         gold_cnt += 1
                     q.append((xdn, ydm))
         cnt += 1
-    
+    return (gold, gold_cnt)
+
 ans = 0
 for x in range(N):
     for y in range(N):
         for i in range(len(bfs_count)):
-
             g = bfs(x, y, bfs_count[i])
             cost = i * i + (i + 1) * (i +1)
             if g[0] > cost:
