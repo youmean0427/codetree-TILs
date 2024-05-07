3b1350913e716d26b9c69893f759a1c457149c93
YM <109326287+youmean0427@users.noreply.github.com>
2024-05-07 18:47:30 +0900

    Title: 금 채굴하기, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240507/금 채굴하기/gold-mining.py
+++ 240507/금 채굴하기/gold-mining.py
@@ -1,55 +1,22 @@
 n, m  = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-bfs_count = [0, 1]
-start = 4
-while len(bfs_count) != 20:
-    bfs_count.append(bfs_count[-1] + start)
-    start += 4
-
-def bfs(sn, sm, c):
-    dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    visited = [[0 for _ in range(n)] for _ in range(n)]
-    visited[sn][sm] = 1
-    visited_dict = {}
-   
+def gold(i, j, k):
     gold = 0
-    gold_count = 0
-    if arr[sn][sm] == 1:
-        gold = 1
-        gold_count = 1
-
-    cnt = 0
-    q = [(sn, sm)]
-
-    while q:
-        if c == cnt:
-            return (gold, gold_count)
-        
-        x, y = q.pop(0)
-
-        for dn, dm in dir:
-            dnx , dmy = dn + x, dm + y
-
-            if 0 <= dnx < n and 0 <= dmy < n:
-                if visited[dnx][dmy] == 0:
-                    if arr[dnx][dmy] == 1:
-                        gold_count += 1
-                        gold += m
-                    visited[dnx][dmy] = 1
-                    q.append((dnx, dmy))
-            else:
-                if (dnx, dmy) not in visited_dict:
-                    visited_dict[(dnx, dmy)] = 1
-                    q.append((dnx, dmy))
-        cnt += 1
+    for a in range(n):
+        for b in range(n):
+            if abs(a-i) + abs(b-j) <= k:
+                if arr[a][b] == 1:
+                    gold += 1
+    return gold
 
 ans = 0
-for x in range(n):
-    for y in range(n):
-        for i in range(len(bfs_count)):
-            g = bfs(x, y, bfs_count[i])
-            cost = i * i + (i + 1) * (i + 1)
-            if g[0] >= cost:
-                ans = max(ans, g[1])
+for i in range(n):
+    for j in range(n):
+        for k in range(2 * (n-1) + 1):
+            g = gold(i, j, k)
+            gc = g * m
+            cost = k * k + (k + 1) * (k + 1)
+            if gc > cost:
+                ans = max(ans, g)
 print(ans)
