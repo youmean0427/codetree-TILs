0852b8465576551d2e96820103500fb74340e454
YM <109326287+youmean0427@users.noreply.github.com>
2024-05-09 22:30:19 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240509/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 240509/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -1,27 +1,30 @@
 n, r, c = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-
-dir = {1 : (-1, 0), 2 : (1, 0), 3 : (0, -1), 4 : (0, 1)}
-r -= 1
-c -= 1
+r, c = r - 1, c- 1
 ans = [arr[r][c]]
 
-def dfs(r, c):
+def is_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+        
+def can_go(x, y, now):
+    return is_range(x, y) and arr[x][y] > now
+
+def move(r, c):
+    dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+    for dx, dy in zip(dxs, dys):
+        dxr, dyc = dx + r, dy + c
+        if can_go(dxr, dyc, arr[r][c]):
+            ans.append(arr[dxr][dyc])
+            return (dxr, dyc)
     
-    stack = [(r, c)]
-    while stack:
-        x, y = stack.pop()
-        max_value = 0
+    return False
 
-        for idx in dir.keys():
-            dnx, dmy = dir[idx][0] + x, dir[idx][1] + y
+while True:
+    x = move(r, c)
+    if x:
+        r, c = x[0], x[1]
+    else:
+        break
 
-            if 0 <= dnx < n and 0 <= dmy < n:
-                if arr[dnx][dmy] > arr[x][y]:
-                    max_value = arr[dnx][dmy]
-                    stack.append((dnx, dmy))
-                    ans.append(max_value)
-                    break
-
-dfs(r, c)
 print(*ans)
