8e09559ac6b4713cca6797924930a94f815090ee
YM <109326287+youmean0427@users.noreply.github.com>
2024-05-11 20:52:46 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 134ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240511/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -0,0 +1,51 @@
+n, m, t = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+balls = []
+balls_cnt = [[0 for _ in range(n)] for _ in range(n)]
+for _ in range(m):
+    x, y = map(int, input().split())
+    balls.append((x-1, y-1))
+
+
+def check_ball_cnt(arr):
+
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] >= 2:
+                arr[i][j] = 0
+    return arr
+
+
+def find():
+    move_arr = [[0 for _ in range(n)] for _ in range(n)]
+    dir = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+
+    while balls:
+        bn, bm = balls.pop(0)
+
+        m_b = 0
+        for dn, dm in dir[::-1]:
+            bdn, bdm = bn + dn, bm + dm
+            if 0 <= bdn < n and 0 <= bdm < n:
+                if arr[bdn][bdm] > m_b:
+                    m_b = arr[bdn][bdm]
+                    next_pos = (bdn, bdm)
+        
+        move_arr[next_pos[0]][next_pos[1]] += 1
+    
+   
+    balls_cnt = check_ball_cnt(move_arr)
+
+    ans = 0
+    for i in range(n):
+        for j in range(n):
+            if balls_cnt[i][j]:
+                balls.append((i, j))
+                ans += 1
+
+    return ans
+
+while t > 0:
+    ans = find()
+    t -= 1
+print(ans)
