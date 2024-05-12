52a790d0129ef23cc3032639db7d35a430a398ac
YM <109326287+youmean0427@users.noreply.github.com>
2024-05-12 22:19:25 +0900

    Title: 벽이 있는 충돌 실험, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240512/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 240512/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -41,7 +41,7 @@ for _ in range(T):
         ball_x, ball_y = int(ball[0])-1, int(ball[1])-1
         arr[ball_x][ball_y] = ball[2]
     
-    for _ in range(n*n):
+    for _ in range(n):
         next_arr = ball_move()
         arr = next_arr
 
