01b5fd5cb59f082ac6f73a4e1cb4bcff0d92552c
YM <109326287+youmean0427@users.noreply.github.com>
2024-05-13 14:23:33 +0900

    Title: 1차원 폭발 게임, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240513/1차원 폭발 게임/The-1D-bomb-game.py
@@ -0,0 +1,30 @@
+n, m = map(int, input().split())
+arr = []
+
+for _ in range(n):
+    arr.append(int(input()))
+
+cnt = 0
+for _ in range(n):
+    new_arr = []
+    cnt = 0
+    for i in range(0, len(arr)):
+        if new_arr:
+            if new_arr[-1] == arr[i]:
+                if i == len(arr) -1:
+                    new_arr.pop()
+                    break
+                cnt = 1
+                continue
+        if cnt == 1:
+            cnt = 0
+            new_arr.pop() 
+        new_arr.append(arr[i])
+    arr = new_arr
+
+if arr:
+    print(len(arr))
+    for i in arr:
+        print(i)
+else:
+    print(0)
