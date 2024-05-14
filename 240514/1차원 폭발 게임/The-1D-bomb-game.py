e3aee682e4c75e53131f7799e81d0aa2849da8e6
YM <109326287+youmean0427@users.noreply.github.com>
2024-05-14 17:20:53 +0900

    Title: 1차원 폭발 게임, Time: 126ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240514/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240514/1차원 폭발 게임/The-1D-bomb-game.py
@@ -11,13 +11,14 @@ for _ in range(n):
 
     start = 0
     end = 0
-    
+    cnt = False
     boom = []
 
     while i <= j and j < len(arr):
 
         if arr[i] == arr[j]:
             start = i 
+            cnt = True
             j += 1
 
             if j >= len(arr):
@@ -26,13 +27,13 @@ for _ in range(n):
                     break
 
         else:
-            if start:
+            if cnt:
                 if j-1 - i + 1 >= m: 
                     end = j-1
                     boom.append((start, end))
                     start = 0
                     end = 0
-                    
+                    cnt = False
                     i = j
                     j = i+1
                     continue
@@ -44,7 +45,6 @@ for _ in range(n):
     for x, y in boom:
         for i in range(x, y+1):
             arr[i] = 0
-
 
     new_arr = []
     for i in arr:
