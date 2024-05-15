4140e01aa59d6e70ebcf9d45633632daa691f7a8
YM <109326287+youmean0427@users.noreply.github.com>
2024-05-15 18:59:20 +0900

    Title: 1차원 윷놀이, Time: 1400ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240515/1차원 윷놀이/yutnori-1d.py
@@ -0,0 +1,26 @@
+n, m, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+
+def back(cnt, sm):
+    global ans
+
+    if cnt >= n:
+        total = 0
+        for i in sm:
+            if sm[i] >= m:
+                total += 1
+        ans = max(ans, total)
+        return 
+
+    for i in range(1, k+1):
+        sm[i] = sm[i] + arr[cnt]
+        back(cnt+1, sm)
+        sm[i] = sm[i] - arr[cnt]
+
+sm = {}
+ans = 0
+for i in range(1, k+1):
+    sm[i] = 1 
+back(0, sm)
+print(ans)
