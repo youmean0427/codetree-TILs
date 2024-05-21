69b04560dd366ae655c682a7510dd5e8eb65197f
YM <109326287+youmean0427@users.noreply.github.com>
2024-05-21 23:56:00 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240521/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240521/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -1,4 +1,21 @@
-n, m = map(int, input().splite())
-arr = [list(map(int, input().splite())) for _ in range(n)]
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0 for _ in range(n)] for _ in range(m)]
+dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+def bfs(sn, sm):
 
-print(arr)
+    q = [(sn, sm)]
+    while q:
+        x, y = q.pop(0)
+        for dn, dm in dir:
+            xn, ym  = x + dn, y + dm 
+            if 0 <= xn < n and 0 <= ym < m:
+                if arr[xn][ym] == 1 and visited[xn][ym] == 0:
+                    visited[xn][ym] = 1
+                    q.append((xn, ym))
+
+bfs(0, 0)
+if visited[n-1][m-1]:
+    print(1)
+else:
+    print(0)
