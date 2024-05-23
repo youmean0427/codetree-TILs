0ae7774dad8e431d26d780101e8b913434db04e1
YM <109326287+youmean0427@users.noreply.github.com>
2024-05-23 21:41:33 +0900

    Title: 비를 피하기, Time: 273ms, Memory: 19MB, Status: Passed - CodeTree

--- 240523/비를 피하기/stay-out-of-rain.js
+++ 240523/비를 피하기/stay-out-of-rain.js
@@ -4,29 +4,25 @@ const input = fs.readFileSync(0).toString().trim().split('\n')
 const [n, h, m] = input[0].split(" ").map(x => parseInt(x))
 const arr = input.slice(1, n+1).map(x => x.split(" ").map(x => parseInt(x)))
 
-const people = []
-
+// 비를 피할 수 있는 공간(3)에서 시작해서 사람(2)를 찾는 방법
+const start = []
+let visited = Array.from(new Array(n), () => Array(n).fill(0))
 for (let i = 0; i < n; i++) {
     for (let j =0; j < n; j++) {
-        if (arr[i][j] === 2) {
-            people.push([i, j])
+        if (arr[i][j] === 3) {
+            start.push([i, j])
+            visited[i][j] = 1
         }
     }
 }
 
 dir = [[0, 1], [1, 0], [-1, 0], [0, -1]]
 ans = Array.from(new Array(n), () => Array(n).fill(0))
-function bfs(sn, sm) {
-    q = [[sn, sm]]
-    let visited = Array.from(new Array(n), () => Array(n).fill(0))
-    visited[sn][sm] = 1
+function bfs() {
+    q = [...start]
+
     while (q.length) {
         const [x, y] = q.shift()
-
-        if (arr[x][y] === 3) {
-            ans[sn][sm] = visited[x][y] - 1 
-            return 
-        }
 
         dir.forEach(d => {
             xd = x + d[0];
@@ -42,13 +38,25 @@ function bfs(sn, sm) {
                 }
         })
     }
-    ans[sn][sm] = -1
-    return
 }
 
-people.forEach(x => {
-    bfs(x[0], x[1])
-})
-ans.forEach(x => {
+
+bfs()
+
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+        if (visited[i][j] && arr[i][j] === 2) {
+            visited[i][j] -= 1
+        }
+        else if (visited[i][j] === 0 && arr[i][j] === 2) {
+            visited[i][j] = -1
+        }
+        else if (arr[i][j] == 0 || arr[i][j] == 3) {
+            visited[i][j] = 0
+        }
+    }
+}
+
+visited.forEach(x => {
     console.log(...x)
 })
