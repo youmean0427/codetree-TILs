a7d88da08ab5f37464a4ace6ebd26c9a79f26000
YM <109326287+youmean0427@users.noreply.github.com>
2024-05-26 21:08:57 +0900

    Title: 변수 출력하기 3, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240526/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+        char c = 'C';
+        String d = "->";
+        System.out.println(a + d + b + d + c);
+    }
+}
