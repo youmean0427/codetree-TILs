1ed85951541e5a987f067ce42aed49c060f366cd
YM <109326287+youmean0427@users.noreply.github.com>
2024-05-27 17:26:36 +0900

    Title: 합과 차의 나눗셈, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

--- 240527/합과 차의 나눗셈/divide-of-sum-and-sub.java
+++ 240527/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        double c = ((double)a + b) / (a - b);
+        double c = (double)(a + b) / (a - b);
         System.out.printf("%.2f", c);
     }
 }
