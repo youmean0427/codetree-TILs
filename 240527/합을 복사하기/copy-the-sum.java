397f20822711b082fb9706261122f7401e918533
YM <109326287+youmean0427@users.noreply.github.com>
2024-05-27 14:44:06 +0900

    Title: 합을 복사하기, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240527/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1, b = 2, c = 3;
+        a = c = b = a + b + c;
+        System.out.print(a + " " + b + " "+ c);
+    }
+}
