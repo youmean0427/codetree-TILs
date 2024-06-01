3950971bb3ed98b0eb8dd620065a73cd5fea023c
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-01 17:54:37 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 171ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240601/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -0,0 +1,35 @@
+from collections import deque
+N = int(input())
+
+visited = [float('inf')] * (2*N)
+def bfs(n):
+    
+    visited[n] = 1
+    q = deque([[n, 0]])
+
+    while q:
+        x, cnt = q.popleft()
+       
+        if x == 1:
+            print(visited[x])
+            return
+
+        if visited[x-1] > cnt+1:
+            visited[x-1] = cnt+1
+            q.append([x-1, cnt+1])
+
+        if visited[x+1] > cnt+1:
+            visited[x-1] = cnt+1
+            q.append([x+1, cnt+1])
+
+        if x % 2 == 0:
+            if visited[x//2] > cnt+1: 
+                visited[x//2] = cnt+1
+                q.append([x // 2, cnt+1])
+                
+        if x % 3 == 0:
+            if visited[x//3] > cnt + 1:
+                visited[x//3] = cnt+1
+                q.append([x // 3, cnt+1])
+    
+bfs(N)
