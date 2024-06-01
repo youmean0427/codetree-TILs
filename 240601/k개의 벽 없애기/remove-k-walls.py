1a5486b4308309d9f8e8c3d31c3d7dcc5b5af733
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-01 17:30:32 +0900

    Title: k개의 벽 없애기, Time: 167ms, Memory: 25MB, Status: Passed - CodeTree

--- 240601/k개의 벽 없애기/remove-k-walls.py
+++ 240601/k개의 벽 없애기/remove-k-walls.py
@@ -1,4 +1,5 @@
 from collections import deque
+from itertools import combinations
 
 n, k = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
@@ -9,15 +10,24 @@ r2, c2 = map(int, input().split())
 visited = [[float('inf') for _ in range(n)] for _ in range(n)]
 dir = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
-def bfs(sn, sm, w):
 
+walls = []
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == 1:
+            walls.append((i, j))
+
+walls_case = list(combinations(walls, k))
+
+def bfs(sn, sm, arr):
+    visited = [[float('inf') for _ in range(n)] for _ in range(n)]
     q = deque([])
-    q.append((sn, sm, w))
+    q.append((sn, sm))
     visited[sn][sm] = 1
 
     while q:
 
-        x, y, wall = q.popleft()
+        x, y = q.popleft()
 
         for dn, dm in dir:
             dx = dn + x
@@ -26,16 +36,20 @@ def bfs(sn, sm, w):
             if 0 <= dx < n and 0 <= dy < n:
                 if arr[dx][dy] == 0 and visited[dx][dy] >= visited[x][y] + 1:
                     visited[dx][dy] = visited[x][y] + 1
-                    q.append((dx, dy, wall))
-                elif arr[dx][dy] == 1 and visited[dx][dy] >= visited[x][y] + 1:
-                    if wall < k:
-                        visited[dx][dy] = visited[x][y] + 1
-                        q.append((dx, dy, wall+1))
-                        
+                    q.append((dx, dy))                     
+    return visited
 
+ans = float('inf')
+for w in walls_case:
+    wall_arr = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            wall_arr[i][j] = arr[i][j]
+    for k in w:
+        wall_arr[k[0]][k[1]] = 0
 
-bfs(r1-1, c1-1, 0)
-ans = visited[r2-1][c2-1]-1
+    res = bfs(r1-1, c1-1, wall_arr)
+    ans = min(ans, res[r2-1][c2-1] - 1)
 
 if ans == float('inf'):
     print(-1)
