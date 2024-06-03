63481c97377678c728f814095f3bc547c756c249
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-04 00:04:25 +0900

    Title: 단 한 번의 2048 시도, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240604/단 한 번의 2048 시도/one-trial-of-2048-game.py
+++ 240604/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -35,6 +35,7 @@ def line_sum(line):
     gravity = []
     start_idx = 0
     end_idx = 1
+    jump = 0
     while end_idx < 4:
         if line[start_idx] == line[end_idx]:
             line[start_idx]= 2 * line[start_idx]
@@ -43,6 +44,11 @@ def line_sum(line):
             end_idx = start_idx + 1
         elif line[start_idx] and line[end_idx] == 0:
             end_idx += 1
+            jump = 1
+        elif jump:
+            jump = 0
+            start_idx = end_idx
+            end_idx = start_idx + 1
         else:
             start_idx += 1
             end_idx += 1
