f11a2754194f4337dd1c825557b64da6d8379bdc
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-04 17:32:40 +0900

    Title: 시험 통과 여부 확인하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240604/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n >= 80) {
+            System.out.println("pass");
+        } else {
+            System.out.print((80-n) + " more score");
+        }
+
+    }
+}
