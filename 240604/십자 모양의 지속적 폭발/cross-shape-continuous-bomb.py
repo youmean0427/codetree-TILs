020921bbb01ff54a6dd3b0cce9ad56d9e19fb7af
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-04 16:52:03 +0900

    Title: 십자 모양의 지속적 폭발, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240604/십자 모양의 지속적 폭발/cross-shape-continuous-bomb.py
@@ -0,0 +1,70 @@
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+col = []
+for _ in range(m):
+    col.append(int(input())-1)
+
+def temp_copy():
+    temp = [[0  for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            temp[i][j] = arr[i][j]
+    return temp
+
+def explode(sn, sm):
+    global arr
+    temp = temp_copy()
+    
+    dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    cnt = 1
+    temp[sn][sm] = 0
+    while cnt < arr[sn][sm]:
+        for d in dir:
+            dn = sn + d[0] * cnt
+            dm = sm + d[1] * cnt
+
+            if 0 <= dn < n and 0 <= dm < n:
+                temp[dn][dm] = 0
+        cnt += 1
+    g_arr = gravity(temp)
+    arr = g_arr
+
+def gravity(g_arr):
+
+    total = []
+    for j in range(n):
+        temp = []
+        for i in range(n-1, -1, -1):
+            if g_arr[i][j] == 0:
+                continue
+            else:
+                temp.append(g_arr[i][j])
+
+        while len(temp) < n:
+            temp.append(0)
+        total.append(temp)
+            
+    for i in range(n):
+        for j in range(n):
+            g_arr[(n-1)-j][i] = total[i][j]
+ 
+    return g_arr
+
+            
+row = 0
+type = 0
+for c in col:
+
+    while arr[row][c] == 0:
+        row += 1
+        if row >= n:
+            type = 1
+            break
+
+    if type == 0:
+        explode(row, c)
+    
+
+for a in arr:
+    print(*a)
