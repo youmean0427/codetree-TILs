1b74ef952275edad2ab4fb4a173410d15868d86e
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-06 22:30:15 +0900

    Title: 범위 안의 수, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240606/범위 안의 수/number-in-range.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a = sc.nextInt();
+
+        if ( a >= 10 && a <= 20) {
+            System.out.print("yes");
+        } else {
+            System.out.print("no");
+        }
+
+    }
+}
