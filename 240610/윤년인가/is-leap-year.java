4da8431e87e4eb20c28bf1193fbf8e219cb886df
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-10 00:30:09 +0900

    Title: 윤년인가, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240610/윤년인가/is-leap-year.java
+++ 240610/윤년인가/is-leap-year.java
@@ -6,7 +6,7 @@ public class Main {
 
         int y = sc.nextInt();
 
-        if (y % 4 == 0 || (y % 100 == 0 && y % 400 != 0)) {
+        if (y % 4 == 0 && (y % 100 != 0 && y % 400 == 0)) {
             System.out.print("true");
         } else {
             System.out.print("false");
