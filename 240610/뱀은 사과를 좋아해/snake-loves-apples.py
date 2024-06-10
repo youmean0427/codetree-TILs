15d3cec6c4af14b8d0f71535541588b70e4a3ce0
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-10 18:11:04 +0900

    Title: 뱀은 사과를 좋아해, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240610/뱀은 사과를 좋아해/snake-loves-apples.py
@@ -0,0 +1,60 @@
+n, m, k = map(int, input().split())
+apple = []
+snake = []
+for _ in range(m):
+    line = list(map(int, input().split()))
+    apple.append((line[0]-1, line[1]-1))
+for _ in range(k):
+    line = list(input().split())
+    snake.append((line[0], int(line[1])))
+
+arr = [[0 for _ in range(n)] for _ in range(n)]
+now_temp = [[0 for _ in range(n) for _ in range(n)]]
+temp = [[0 for _ in range(n)] for _ in range(n)]
+
+visited = [(0, 0)]
+
+for ax, ay in apple:
+    arr[ax][ay] = 1
+
+time = 0
+dir = {"R": (0, 1), "L": (0, -1), "U": (-1, 0), "D": (1, 0)}
+for d, c in snake:
+    for _ in range(c):
+        time += 1
+        now_visited = []
+        temp = [[0 for _ in range(n)] for _ in range(n)]
+        x, y = visited.pop(0)
+        now_x, now_y = x, y
+        
+        dx, dy = x + dir[d][0], y + dir[d][1]
+ 
+        if (0 <= dx < n and 0 <= dy < n):
+            
+            if now_temp[dx][dy] == 1:
+                print(time)
+                exit(0)
+
+            now_visited.append((dx, dy))
+            temp[dx][dy] = 1
+
+
+            while visited:
+                vx, vy = visited.pop(0)
+                now_visited.append((now_x, now_y))
+                temp[now_x][now_y] = 1
+                now_x, now_y = vx, vy
+
+            if (arr[dx][dy] == 1):
+                temp[now_x][now_y] = 1
+                now_visited.append((now_x, now_y))
+        else:
+            print(time)
+            exit(0)
+            break
+
+        now_temp = temp
+        visited = now_visited
+    
+
+print(time)
