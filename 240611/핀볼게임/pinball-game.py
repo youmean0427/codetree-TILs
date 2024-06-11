349cc1fc6f42125db2ef5da020a5a688705443bc
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-11 18:33:50 +0900

    Title: 핀볼게임, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

--- 240611/핀볼게임/pinball-game.py
+++ 240611/핀볼게임/pinball-game.py
@@ -15,10 +15,10 @@ for i in range(n):
             start.append((i, j, "U"))
         
         if j == 0:
-            start.append((i, j, "L"))
+            start.append((i, j, "R"))
         
         if j == n-1:
-            start.append((i, j, "R"))
+            start.append((i, j, "L"))
             
 dir = {"R" : (0, 1), "L" : (0, -1), "U": (-1, 0), "D": (1, 0)}
 wall = [{}, {"D": "L", "L": "D", "U": "R", "R": "U"}, {"D": "R", "R": "D", "U": "L", "L": "U"}]
@@ -29,8 +29,15 @@ def ball (sn, sm, d):
     now_n = sn
     now_m = sm
     now_d = d
-    while True:
 
+    if (arr[now_n][now_m] == 1):
+        now_d = wall[1][now_d]
+    elif (arr[now_n][now_m] == 2):
+        now_d = wall[2][now_d]
+
+    cnt = 0
+    while cnt < n*n:
+        
         dn, dm = now_n + dir[now_d][0], now_m + dir[now_d][1]
 
         if (0 <= dn < n and 0 <= dm < n):
@@ -48,6 +55,9 @@ def ball (sn, sm, d):
             time += 1
             return time
         
+        cnt += 1
+    return time
+
 ans = 0
 for a, b, c in start:
     x = ball(a, b, c)
