4e34b5d38d203c01ed18c40bc6455b59eab3fb7c
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-15 19:35:47 +0900

    Title: 점 빼기, Time: 1606ms, Memory: 40MB, Status: Passed - CodeTree

--- 240615/점 빼기/remove-point.py
+++ 240615/점 빼기/remove-point.py
@@ -2,25 +2,18 @@ from sortedcontainers import SortedSet
 
 n, m = map(int, input().split())
 s = SortedSet()
-pos = {}
+
 for _ in range(n):
     x, y = map(int, input().split())
-    s.add(x)
-    if x in pos:
-        pos[x].append(y)
-        pos[x].sort()
-    else:
-        pos[x] = [y]
+    s.add((x, y))
 
 for _ in range(m):
     k = int(input())
-    idx = s.bisect_left(k)
-    
-    if idx >= len(s):
+    # tuple도 활용 가능
+    idx = s.bisect_left((k, -1))
+
+    if idx == len(s):
         print(-1, -1)
     else:
-        print(s[idx], pos[s[idx]][0])
-        pos[s[idx]].pop(0)
-
-        if len(pos[s[idx]]) == 0:
-            s.remove(s[idx])
+        print(s[idx][0], s[idx][1])
+        s.remove(s[idx])
