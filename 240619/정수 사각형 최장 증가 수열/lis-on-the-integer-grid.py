de3dbe72f192621847b5d0f9c831ccd2b9f4be9b
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-19 23:04:31 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 241ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240619/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -0,0 +1,26 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [[1 for _ in range(n)] for _ in range(n)]
+
+
+dir = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
+for _ in range(2):
+    for i in range(n):
+        for j in range(n):
+            
+            step = 1
+            for dn, dm in dir:
+                dnx, dmy = dn + i, dm + j
+                
+                if 0 <= dnx < n and 0 <= dmy < n:
+                    if arr[i][j] > arr[dnx][dmy]:
+                        step = max(step, dp[dnx][dmy] + 1)
+            
+            dp[i][j] = step
+
+ans = 0
+for i in dp:
+    ans = max(ans, max(i))
+print(ans)
