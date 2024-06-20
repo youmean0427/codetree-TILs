2fc67a7f7b2f85e9899e168b64b561ee6fd39453
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-20 14:17:05 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240620/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -0,0 +1,18 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [[float('inf') for _ in range(n)] for _ in range(n)]
+
+
+for i in range(n):
+    for j in range(n):
+        res = []
+        for dn, dm in [(0, -1), (-1, 0)]:
+            dni, dmj = dn + i, dm + j
+            if 0 <= dni < n and 0 <= dmj < n:
+                x = min(dp[i][j], dp[dni][dmj], arr[i][j])
+                res.append(x)
+        if res:
+            dp[i][j] = max(res)
+
+print(dp[n-1][n-1])
