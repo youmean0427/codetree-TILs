d2ea64aeab8f9c05ac9f72bdfe594c35ccc09e95
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-20 14:59:34 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 144ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240620/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240620/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -14,6 +14,8 @@ for i in range(n):
         up_v = float('inf')
         left_v = float('inf')
 
+        # 현재 칸을 포함하여, 위쪽과 왼쪽의 최댓값-최솟값 차이를 기반으로 현재 칸의 차이를 계산
+
         # 위쪽
         if 0 <= i-1 < n:
 
@@ -42,19 +44,23 @@ for i in range(n):
             left_v = abs(max_v-min_v)
             left_p = [min_v, max_v]
 
-
         if left_v > up_v:
             min_dp[i][j] = up_v
             both_dp[i][j] = up_p 
         elif up_v > left_v:
             min_dp[i][j] = left_v
             both_dp[i][j] = left_p
+        
+        # 같을 경우, 현재 칸을 반영하지 않은 위쪽과 왼쪽 칸의 차이를 비교함
         else:
             if up_v == float('inf'):
                 pass
             else:
-                min_dp[i][j] = up_v
-                both_dp[i][j] = up_p
-
+                if min_dp[i-1][j] > min_dp[i][j-1]:
+                    min_dp[i][j] = left_v
+                    both_dp[i][j] = left_p
+                else:
+                    min_dp[i][j] = up_v
+                    both_dp[i][j] = up_p
 
 print(min_dp[n-1][n-1])
