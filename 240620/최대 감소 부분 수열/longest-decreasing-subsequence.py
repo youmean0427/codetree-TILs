fd00b3f0f0278891e9027dfb0448c2e218412e2d
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-20 17:22:48 +0900

    Title: 최대 감소 부분 수열, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 240620/최대 감소 부분 수열/longest-decreasing-subsequence.py
+++ 240620/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -6,9 +6,10 @@ dp = [1] * n
 
 for i in range(1, n):
     
-    max_v = 1
+    max_v = 0
     for j in range(i):
         if arr[j] > arr[i]:
             max_v = max(max_v, dp[j])
-    dp[i] += max_v 
+    dp[i] = max_v + 1 
+    
 print(max(dp))
