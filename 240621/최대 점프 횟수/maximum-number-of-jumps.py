34a8ab6cf5c4ffb840079b956f7690bac487d9d3
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-21 18:50:29 +0900

    Title: 최대 점프 횟수, Time: 124ms, Memory: 25MB, Status: Passed - CodeTree

--- 240621/최대 점프 횟수/maximum-number-of-jumps.py
+++ 240621/최대 점프 횟수/maximum-number-of-jumps.py
@@ -1,17 +1,26 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-dp = [float('-inf')] * n
+# dp[i]는 i까지의 최대 점프 횟수
+dp = [0] * n
+# 첫번째 위치에서 시작했음을 표시
+dp[0] = 1
 
-dp[0] = 0
 
 for i in range(1, n):
     max_v = 0
     for j in range(i):
-        if dp[j] == float('-inf'):
+
+        # 첫번째 위치를 고정하기 위해
+        # 아래 코드가 없으면, 시작점이 변경 
+        if dp[j] == 0:
             continue
 
+        # 현재 칸(i)까지 점프를 할 수 있는 경우
+        # 최대값을 찾아냄, 점프 횟수 + 1
         if j + arr[j] >= i:
-            dp[i] = max(dp[i], dp[j] + 1)
-        
-print(max(dp))
+            max_v = max(max_v, dp[j]+1)
+
+    dp[i] = max_v
+
+print(max(dp)-1)
