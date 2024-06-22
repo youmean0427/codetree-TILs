f6358671e154f20b37947a3595999ff7a445ce92
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-22 13:07:59 +0900

    Title: 2차원 최대 증가 수열, Time: 190ms, Memory: 25MB, Status: Passed - CodeTree

--- 240622/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240622/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -6,32 +6,25 @@ dp = [[0 for _ in range(m)] for _ in range(n)]
 # 초기값
 dp[0][0] = 1
 
-# # 가로 초기값
-# for i in range(1, m):
-#     for j in range(i):
-#         if arr[0][i] > arr[0][j]:
-#             dp[0][i] = max(dp[0][i], dp[0][j]+1)
-
-# print(dp)
-
-# # 세로 초기값
-
-# for i in range(1, n):
-#     for j in range(i):
-#         if arr[i][0] > arr[j][0]:
-#             dp[i][0] = max(dp[i][0], dp[j][0] + 1)
-
-# print(dp)
-
 for i in range(1, n):
     for j in range(1, m):
 
         for x in range(0, i):
             for y in range(0, j):
 
-                if arr[i][j] > arr[x][y] and arr[0][0] < arr[x][y]:
+                # 시작점이 변경될 수 있음
+                # dp[x][y] > 0 조건으로 (0, 0)에서 출발했음을 판단
+                if arr[i][j] > arr[x][y] and dp[x][y] > 0:
                     dp[i][j] = max(dp[i][j], dp[x][y] + 1 )
 ans = 0
 for i in dp:
     ans = max(ans, max(i))
 print(ans)
+
+# TEST CASE 6
+
+# 4 4
+# 9 1 1 1
+# 1 2 1 4
+# 1 1 3 1
+# 1 1 1 5
