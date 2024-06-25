df60ec48701b315aca185fa70fecf7f3c6cf1b20
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-25 17:52:25 +0900

    Title: 배낭 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240625/배낭 채우기/knapsack.py
+++ 240625/배낭 채우기/knapsack.py
@@ -10,9 +10,9 @@ for i in range(1, n):
         if j < arr[i][0]:
             dp[i][j] = dp[i-1][j]
         else:
-            dp[i][j] = max(dp[i][j], dp[i-1][j-arr[i][0]] + arr[i][1])
+            dp[i][j] = max(dp[i][j], dp[i-1][j-arr[i][0]] + arr[i][1], dp[i-1][j])
 
 ans = 0
-for i in range(n):
-    ans = max(ans, dp[i][m])
+for i in dp:
+    ans = max(ans, max(i))
 print(ans)
