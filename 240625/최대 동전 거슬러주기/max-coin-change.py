c798330fa7c34edc31b63606f215041ee18cf145
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-25 17:13:22 +0900

    Title: 최대 동전 거슬러주기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240625/최대 동전 거슬러주기/max-coin-change.py
+++ 240625/최대 동전 거슬러주기/max-coin-change.py
@@ -1,7 +1,8 @@
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-dp = [0] * (m+1)
+dp = [float('-inf')] * (m+1)
+dp[0] = 0
 
 # 동전의 합
 for i in range(m+1):
@@ -9,6 +10,7 @@ for i in range(m+1):
     for j in range(n):
         if i >= arr[j]:
             dp[i] = max(dp[i], dp[i-arr[j]] + 1)
+
 
 if dp[m]:
     print(dp[m])
