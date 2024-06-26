24ef3dd8a5b818860974baf059ec7ef6bec1f774
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-26 15:50:29 +0900

    Title: 정수 n개의 합 3, Time: 150ms, Memory: 28MB, Status: Passed - CodeTree

--- 240626/정수 n개의 합 3/sum-of-n-integers-3.py
+++ 240626/정수 n개의 합 3/sum-of-n-integers-3.py
@@ -8,15 +8,8 @@ for i in range(1, n+1):
     for j in range (1, n+1):
         dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + arr[i-1][j-1] 
 
-for i in range(n+1):
-    for j in range(n+1):
-        res[i][j] = dp[i][j]
-
+ans = 0
 for i in range(k, n+1):
     for j in range(k, n+1):
-        res[i][j] = res[i][j] - dp[i-k][j] - dp[i][j-k] + dp[i-k][j-k]
-
-ans = 0
-for i in res:
-    ans = max(ans, max(i))
+        ans = max(ans , dp[i][j] - dp[i-k][j] - dp[i][j-k] + dp[i-k][j-k])
 print(ans)
