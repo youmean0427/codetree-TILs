1a02c96e5df4439e871ced1b175ca5c68194f260
YM <109326287+youmean0427@users.noreply.github.com>
2024-06-26 15:12:19 +0900

    Title: 배낭 채우기 2, Time: 169ms, Memory: 25MB, Status: Passed - CodeTree

--- 240626/배낭 채우기 2/knapsack-2.py
+++ 240626/배낭 채우기 2/knapsack-2.py
@@ -1,23 +1,13 @@
 N, M = map(int, input().split())
 arr= [list(map(int, input().split())) for _ in range(N)]
 
+dp = [float("-inf")] * (M+1)
+dp[0]= 0
+# 무게의 합
+for i in range(M+1):
+    # 보석
+    for j in range(N):
+        if i >= arr[j][0]:
+            dp[i] = max(dp[i], dp[i-arr[j][0]] + arr[j][1])
 
-dp = [[float('-inf') for _ in range(M+1)] for _ in range(N)]
-dp[0][0] = 0
-# 보물
-for i in range(N):
-    w, c = arr[i]
-    # 무게의 합
-    for j in range(M+1):
-        if j >= w:
-            if j % w == 0:
-                dp[i][j] = max(dp[i][j], c * (j // w), dp[i][j - w] + c, dp[i-1][j])
-            else:
-                dp[i][j] = max(dp[i][j], dp[i-1][j], dp[i][j-w] + c)
-        else:
-            dp[i][j] = dp[i-1][j]
-
-ans = 0
-for i in dp:
-    ans = max(ans, max(i))
-print(ans)
+print(max(dp))
