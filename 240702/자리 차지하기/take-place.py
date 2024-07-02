f7bdc4f4a3eef2f744ca8dac38c6ca68a3aae284
YM <109326287+youmean0427@users.noreply.github.com>
2024-07-02 19:16:56 +0900

    Title: 자리 차지하기, Time: 710ms, Memory: 38MB, Status: Passed - CodeTree

--- 240702/자리 차지하기/take-place.py
+++ 240702/자리 차지하기/take-place.py
@@ -3,16 +3,15 @@ from sortedcontainers import SortedSet
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 s = SortedSet([i for i in range(1, m+1)])
-visited = [0] * m
 
-arr.sort()
 cnt = 0
 
 for i in arr:
-
-    idx = s.bisect_right(i)-1
-    if idx >= 0:
-        s.remove(s[idx])
+    idx = s.bisect_right(i)
+    if idx > 0: 
+        s.remove(s[idx - 1])
         cnt += 1
+    else:
+        break
     
 print(cnt)
