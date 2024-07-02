fb3f486f8a5213a6f04c39579d6a4a17fc8bcd85
YM <109326287+youmean0427@users.noreply.github.com>
2024-07-02 18:49:35 +0900

    Title: 최대로 연속한 숫자, Time: 6597ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

--- 240702/최대로 연속한 숫자/maximum-consecutive-number.py
+++ 240702/최대로 연속한 숫자/maximum-consecutive-number.py
@@ -3,7 +3,6 @@ from sortedcontainers import SortedSet
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-s = SortedSet([i for i in range(n+1)])
 
 k = SortedSet()
 k.add(-1)
