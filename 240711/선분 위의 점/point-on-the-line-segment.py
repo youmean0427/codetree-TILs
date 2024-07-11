ab5346705650d64381e0cbc6c8182ae37efc9368
YM <109326287+youmean0427@users.noreply.github.com>
2024-07-11 18:45:21 +0900

    Title: 선분 위의 점, Time: 320ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240711/선분 위의 점/point-on-the-line-segment.py
+++ 240711/선분 위의 점/point-on-the-line-segment.py
@@ -5,14 +5,37 @@ lines = []
 for _ in range(m):
     lines.append(list(map(int, input().split())))
 
-for line in lines:
+# dots를 arr로 두고, 선분의 시작점과 끝점을 찾는 방식
+# Lower Bound를 통해, 찾는 값과 같거나 큰 가장 작은 idx를 구함 
+def binary_sort(target):
 
-    start = line[0]
-    end = line[1]
-    cnt = 0
-    for dot in dots:
-        if start <= dot and dot <= end:
-            cnt += 1
+    start = 0
+    end = n-1
 
+    min_val = n
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if dots[mid] >= target:
+            end = mid - 1
+            min_val = min(min_val, mid)
+        else:
+            start  = mid + 1
     
-    print(cnt)
+    return min_val
+
+
+for s, e in lines:
+
+    start = binary_sort(s)
+    end = binary_sort(e)
+
+    ans = end - start
+    
+    if end < n:
+        if dots[end] == e:
+            ans += 1
+
+
+    print(ans)
