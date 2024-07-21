c391dfb26f6414344fd2da1fd2c17966d08cb2da
YM <109326287+youmean0427@users.noreply.github.com>
2024-07-21 21:40:34 +0900

    Title: 문자 출력하기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240721/문자 출력하기/print-text.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String c = sc.next();
+        for (int i = 1; i <= 8; i++) {
+            System.out.print(c);
+        }
+    }
+}
