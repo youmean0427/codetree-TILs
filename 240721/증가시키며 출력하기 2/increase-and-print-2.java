fc44267e6697bcf92d43268bd8722100c2b76d7a
YM <109326287+youmean0427@users.noreply.github.com>
2024-07-21 21:10:08 +0900

    Title: 증가시키며 출력하기 2, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240721/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+
+        while(a < 27) {
+            System.out.print(a + " ");
+            a++;
+        }
+    }
+}
