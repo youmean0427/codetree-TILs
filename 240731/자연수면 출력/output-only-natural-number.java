fb1ad9215343fddf878e01fda00867e1112f83ec
YM <109326287+youmean0427@users.noreply.github.com>
2024-07-31 17:09:02 +0900

    Title: 자연수면 출력, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240731/자연수면 출력/output-only-natural-number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > 0) {
+            for (int i = 1; i <= b; i++) {
+                System.out.print(a);
+            }
+        }
+        else {
+            System.out.print(0);
+        }
+    }
+}
