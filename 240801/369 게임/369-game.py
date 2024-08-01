54b1ccc3adab45fe4951439ec5938d0dc8336d5e
YM <109326287+youmean0427@users.noreply.github.com>
2024-08-01 17:36:31 +0900

    Title: 369 게임, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240801/369 게임/369-game.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+
+for i in range(1, n+1):
+
+    if (i % 3 == 0 or '3' in str(i) or '6' in str(i) or '9' in str(i)):
+        print(0, end= " ")
+    else:
+        print(i, end = " ")
