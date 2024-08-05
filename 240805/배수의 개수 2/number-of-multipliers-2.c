baaf2d308fee632a0351eb6d36cea72e6b58202d
YM <109326287+youmean0427@users.noreply.github.com>
2024-08-05 17:46:05 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240805/배수의 개수 2/number-of-multipliers-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int cnt = 0;
+    for (int i = 0; i <= 10; i++) {
+        int a;
+        scanf("%d", &a);
+
+        if (a % 2 != 0) {
+            cnt++;
+        }
+    }
+
+    printf("%d", cnt);
+
+    return 0;
+}
