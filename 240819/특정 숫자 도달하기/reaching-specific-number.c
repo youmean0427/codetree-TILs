2f50d9530ed469de6cce0baeaed7ccdb2f606997
YM <109326287+youmean0427@users.noreply.github.com>
2024-08-19 02:14:02 +0900

    Title: 특정 숫자 도달하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240819/특정 숫자 도달하기/reaching-specific-number.c
+++ 240819/특정 숫자 도달하기/reaching-specific-number.c
@@ -4,24 +4,28 @@ int main() {
     
     int arr[10];
     int idx;
+    int total;
     int cnt;
-    int sum;
 
     idx = 0;
-    sum = 0;
     while (idx < 10)
     {
-        scanf("%d ", &arr[idx]);
+        scanf("%d", &arr[idx]);
+        idx++;
+    }
+    idx = 0;
+    cnt = 0;
+    total = 0;
+    while (idx < 10)
+    {
+        if (arr[idx] >= 250)
+        {
+            break;
+        }
+        total += arr[idx];
+        cnt += 1;
         idx++;
     }
 
-    cnt = 0;
-    while (arr[cnt] < 250) {
-        sum += arr[cnt];
-        cnt++;
-    }
-    double avg;
-    avg = (double)sum / (cnt);
-    printf("%d %.1lf", sum, avg);
-    return 0;
+    printf("%d %.1lf", total, (double)total/cnt);
 }
