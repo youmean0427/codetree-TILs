8c8227aed2cc4df9a2b5803e33e8640fec288ff7
YM <109326287+youmean0427@users.noreply.github.com>
2024-09-20 18:37:08 +0900

    Title: 퀵 정렬 구현, Time: 218ms, Memory: 22MB, Status: Wrong Answer - CodeTree

---
+++ 240920/퀵 정렬 구현/implement-quick-sort.js
@@ -0,0 +1,49 @@
+const fs = require("fs")
+const input = fs.readFileSync(0).toString().trim().split("\n")
+
+const N = Number(input[0])
+const arr = input[1].split(" ").map(x => Number(x))
+
+function quick_sort(start, end)
+{
+    const pivot = arr[start]
+
+    let i = start + 1
+    let j = end
+    if (start < end)
+    {
+        while(i <= j)
+        {
+            while (i <= j && arr[i] <= pivot)
+                i++
+            while (i <= j && arr[j] >= pivot)
+                j--
+
+            if (i < j)
+            {
+                temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+            }
+        }
+    }
+    
+    temp = arr[j]
+    arr[j] = arr[start]
+    arr[start] = temp
+
+    return j
+}
+
+function quick(start, end)
+{
+    if (start < end)
+    {
+        idx = quick_sort(start, end)
+        quick_sort(start, idx - 1)
+        quick_sort(idx + 1, end)
+    }
+}
+
+quick(0, N-1)
+console.log(...arr)
