ffd728ed74c62c9dfddc18c0a539685626ddce21
YM <109326287+youmean0427@users.noreply.github.com>
2024-10-09 14:35:29 +0900

    Title: 트리 노드 제거, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 241009/트리 노드 제거/remove-tree-node.py
+++ 241009/트리 노드 제거/remove-tree-node.py
@@ -4,7 +4,7 @@ x = int(input())
 
 link = [[] for _ in range(n)]
 delete = [0 for _ in range(n)]
-nums = []
+del_nums = []
 for i in range(n):
     if (arr[i] != -1):
         link[arr[i]].append(i)
@@ -15,7 +15,7 @@ def dfs(start):
     while (stack):
         x = stack.pop()
         delete[x] = 1
-        nums.append(x)
+        del_nums.append(x)
         for i in link[x]:
             if (delete[i] == 0):
                 stack.append(i)
@@ -24,10 +24,8 @@ dfs(x)
 cnt = 0
 
 for i in range(n):
-    if delete[i] == 0 and len(link[i]) == 1 and link[i][0] in nums:
-        cnt += 1
-
-    if link[i] == [] and delete[i] == 0:
-        cnt += 1
+    if delete[i] == 0 :
+        if (len(link[i]) == 1 and link[i][0] in del_nums) or link[i] == []:
+            cnt += 1
 
 print(cnt)
