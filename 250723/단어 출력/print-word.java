feb81931deaa3a3c18eefca08a8a469a3ab0c930
YM <109326287+youmean0427@users.noreply.github.com>
2025-07-23 21:43:24 +0900

    Title: 단어 출력, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

--- 250723/단어 출력/print-word.java
+++ 250723/단어 출력/print-word.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        System.out.print("World");
+        System.out.print("Hello");
     }
 }
