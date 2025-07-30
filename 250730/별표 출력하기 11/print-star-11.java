1dd188ee43b5c0eaa1b9628906971fedb1f31a1d
YM <109326287+youmean0427@users.noreply.github.com>
2025-07-30 20:19:18 +0900

    Title: 별표 출력하기 11, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250730/별표 출력하기 11/print-star-11.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < 2 * n + 1; i++)
+        {
+            for (int j = 0; j < 2 * n + 1; j++)
+            {
+                if (i % 2 == 1 && j % 2 == 1)
+                {
+                    System.out.print("  ");
+                }
+                else
+                {
+                System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
