af69b5e1977b32895eb928c2e71925d8d14baa02
YM <109326287+youmean0427@users.noreply.github.com>
2025-07-30 21:24:10 +0900

    Title: 모양대로 수 출력하기 2, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250730/모양대로 수 출력하기 2/print-out-numbers-in-specific-shape-2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 2;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <n; j++)
+            {
+                System.out.print(cnt + " ");
+                cnt += 2;
+
+                if (cnt == 10)
+                {
+                    cnt = 2;
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
