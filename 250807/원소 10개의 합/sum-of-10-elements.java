7d7fb5cacdd12bab2ee0617629bcb86729774f32
YM <109326287+youmean0427@users.noreply.github.com>
2025-08-07 20:50:55 +0900

    Title: 원소 10개의 합, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250807/원소 10개의 합/sum-of-10-elements.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        int sum = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        System.out.print(sum);
+    }
+}
