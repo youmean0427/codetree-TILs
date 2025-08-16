287ce21b60a77231b3cae98f677d11d37364cebd
YM <109326287+youmean0427@users.noreply.github.com>
2025-08-16 20:37:05 +0900

    Title: 두 정수에 대한 연산값, Time: 138ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250816/두 정수에 대한 연산값/operational-values-for-two-integers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int[] arr = new int[2];
+        arr[0] = a;
+        arr[1] = b;
+        func(arr);
+        System.out.print(arr[0] + " " + arr[1]);
+    }
+
+    public static void func(int[] arr){
+        if (arr[0] >= arr[1])
+        {
+            arr[0] += 25;
+            arr[1] *= 2;
+        } else
+        {
+            arr[1] += 25;
+            arr[0] *= 2;
+        }
+        return;
+    }
+}
