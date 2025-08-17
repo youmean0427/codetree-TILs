f627c986dbf04b0ec5f2b370761568bbd5fc194b
YM <109326287+youmean0427@users.noreply.github.com>
2025-08-17 21:31:18 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 158ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250817/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        func1(n);
+        func2(n);
+    }
+
+
+    public static void func1(int n)
+    {
+        if (n == 0)
+            return;
+        for (int i = 0; i < n; i++)
+        {
+            System.out.print("* ");
+        }
+        System.out.println();
+        func1(n-1);   
+    }
+
+    public static void func2(int n)
+    {
+        if (n == 0)
+            return;
+        func2(n-1);
+        for (int i = 0; i < n; i++)
+        {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+
+}
