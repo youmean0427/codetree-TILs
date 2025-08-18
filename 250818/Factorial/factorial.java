fbd0570cd58c804d2397171fa27ab3aad36f3395
YM <109326287+youmean0427@users.noreply.github.com>
2025-08-18 21:08:21 +0900

    Title: Factorial, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250818/Factorial/factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(F(n));
+    }
+
+    public static int F(int n)
+    {
+        if (n == 0)
+            return 1;
+        return F(n-1) * n;
+    }
+
+
+}
