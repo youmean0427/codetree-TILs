5733ee1d5878ef9fff5aad647350d660bbc4d4ac
YM <109326287+youmean0427@users.noreply.github.com>
2025-11-09 22:01:30 +0900

    Title: 만나는 그 순간, Time: 263ms, Memory: 29MB, Status: Passed - Codetree

--- 251109/만나는 그 순간/the-moment-we-meet.java
+++ 251109/만나는 그 순간/the-moment-we-meet.java
@@ -4,76 +4,47 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] a = new int[1000001];
-        Arrays.fill(a, Integer.MAX_VALUE);
         int[] b = new int[1000001];
-        Arrays.fill(b, Integer.MIN_VALUE);
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-  
         int time  = 1;
-        int aNow = 0;
         for (int i = 0; i < n; i++)
         {
             char c = sc.next().charAt(0);
             int t = sc.nextInt();
-
-            if (c == 'R') {
-                aNow += 1;            
+            
             for (int j = 1; j <= t; j++)
             {
-                a[time] = aNow;
+                if (c == 'R')
+                    a[time] = a[time - 1] + 1;
+                else
+                    a[time] = a[time - 1] - 1;
                 time++;
-                if (j != t)
-                    aNow++;
-            }
-            }
-            else
-            {
-                aNow -= 1;
-                for (int j = 1; j <= t; j++)
-                {
-                    a[time] = aNow;
-                    time++;
-                    if (j!=t)
-                        aNow--;
-                }
             }
         }
 
+        int timeLim = time;
+
         time  = 1;
-        int bNow = 0;
         for (int i = 0; i < m; i++)
         {
             char c = sc.next().charAt(0);
             int t = sc.nextInt();
 
-            if (c == 'R') {
-                bNow += 1;            
             for (int j = 1; j <= t; j++)
             {
-                b[time] = bNow;
+                if (c == 'R')
+                    b[time] = b[time - 1] + 1;
+                else
+                    b[time] = b[time - 1] - 1;
                 time++;
-                if (j != t)
-                    bNow++;
             }
-            }
-            else
-            {
-                bNow -= 1;
-                for (int j = 1; j <= t; j++)
-                {
-                    b[time] = bNow;
-                    time++;
-                    if (j!=t)
-                        bNow--;
-                }
-            }
+
         }
         
-
         int ans = -1;
-        for (int i = 1; i <= 1000000; i++)
+        for (int i = 1; i < timeLim; i++)
         {
             if (a[i] == b[i])
             {
