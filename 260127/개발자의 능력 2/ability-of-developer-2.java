cb292974ebd3ae757854e5455a645f6278964ccd
YM <109326287+youmean0427@users.noreply.github.com>
2026-01-27 21:17:18 +0900

    Title: 개발자의 능력 2, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 260127/개발자의 능력 2/ability-of-developer-2.java
+++ 260127/개발자의 능력 2/ability-of-developer-2.java
@@ -13,13 +13,26 @@ public class Main {
 
         int[] arr = new int[6];
         arr[0] = ability1;
-        arr[0] = ability1;
-        arr[0] = ability1;
-        arr[0] = ability1;
-        arr[0] = ability1;
-        arr[0] = ability1;
+        arr[1] = ability2;
+        arr[2] = ability3;
+        arr[3] = ability4;
+        arr[4] = ability5;
+        arr[5] = ability6;
 
-        
+        Arrays.sort(arr);
+
+        int x = arr[0] + arr[5];
+        int y = arr[1] + arr[4];
+        int z = arr[2] + arr[3];
+
+        int[] sortArr = new int[3];
+        sortArr[0] = x;
+        sortArr[1] = y;
+        sortArr[2] = z;
+
+        Arrays.sort(sortArr);
+
+        System.out.print(sortArr[2] - sortArr[0]);
 
     }
 }
