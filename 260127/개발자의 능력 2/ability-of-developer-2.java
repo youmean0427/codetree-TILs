d1f4c700281eb8cce1c303da79515df47b171fa5
YM <109326287+youmean0427@users.noreply.github.com>
2026-01-27 21:11:50 +0900

    Title: 개발자의 능력 2, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260127/개발자의 능력 2/ability-of-developer-2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ability1 = sc.nextInt();
+        int ability2 = sc.nextInt();
+        int ability3 = sc.nextInt();
+        int ability4 = sc.nextInt();
+        int ability5 = sc.nextInt();
+        int ability6 = sc.nextInt();
+        
+
+        int[] arr = new int[6];
+        arr[0] = ability1;
+        arr[0] = ability1;
+        arr[0] = ability1;
+        arr[0] = ability1;
+        arr[0] = ability1;
+        arr[0] = ability1;
+
+        
+
+    }
+}
