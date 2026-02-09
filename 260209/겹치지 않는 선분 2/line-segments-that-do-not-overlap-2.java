55c019a7f2b2e7a9645825f985a6a2d59bb09ee4
YM <109326287+youmean0427@users.noreply.github.com>
2026-02-09 21:44:32 +0900

    Title: 겹치지 않는 선분 2, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 260209/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
+++ 260209/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -7,33 +7,44 @@ public class Main {
         int n = sc.nextInt();
         int[] x1 = new int[n];
         int[] x2 = new int[n];
-        int[] x3 = new int[n];
-        int[] x4 = new int[n];
         for (int i = 0; i < n; i++) {
             x1[i] = sc.nextInt();
             x2[i] = sc.nextInt();
-            x3[i] = x1[i];
-            x4[i] = x2[i];
+           
         }
+        int[] cnt = new int[101];
+        
+        // 교점으로 찾기
 
-        Arrays.sort(x3);
-        Arrays.sort(x4);
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i+1; j < n; j++)
+            {
+                double D = (x2[i] - x1[i]) - (x2[j] - x1[j]);
+
+                double t = (x1[j] - x1[i]) / D;
+
+                if (0.0 <= t && t <= 1.0)
+                {
+                    double xx = x1[i] + (x2[i] - x1[i]) * t;
+                    double yy = t;
+
+                    cnt[i] = 1;
+                    cnt[j] = 1;
+                } 
+
+            }
+        }
 
         int ans = 0;
-        for (int i = 0; i < n; i++)
+        for (int k = 0; k < n; k++)
         {
-            for (int j = 0; j < n; j++)
+            if (cnt[k] == 0)
             {
-                if (x3[i] == x1[j])
-                {
-                    if (x2[j] == x4[i])
-                    {
-                        ans++;
-                    }
-                } 
+                ans++;
             }
-            
         }
         System.out.print(ans);
+     
     }
 }
