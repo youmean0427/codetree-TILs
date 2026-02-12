63b25ee2cbbeab778685da491d79878e226c1d93
YM <109326287+youmean0427@users.noreply.github.com>
2026-02-12 21:26:14 +0900

    Title: 데이터센터의 온도 조정 2, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260212/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = sc.nextInt();
+        int g = sc.nextInt();
+        int h = sc.nextInt();
+        int[] ta = new int[n];
+        int[] tb = new int[n];
+        for (int i = 0; i < n; i++) {
+            ta[i] = sc.nextInt();
+            tb[i] = sc.nextInt();
+        }
+        Arrays.sort(ta);
+        Arrays.sort(tb);
+
+        int max = Math.max(ta[n-1], tb[n-1]);
+        int min = Math.min(ta[0], tb[0]);
+        int ans = 0;
+        for (int i = min; i <= max; i++)
+        {
+            int work = 0;
+            for (int j = 0; j < n; j++)
+            {
+
+                if (i < ta[j])
+                    work += c;
+                else if (i >= ta[j] && i <= tb[j])
+                    work += g;
+                else if (i > tb[j])
+                    work += h;
+            }
+            ans = Math.max(work, ans);
+   
+
+        }
+        System.out.print(ans);
+
+    }
+}
