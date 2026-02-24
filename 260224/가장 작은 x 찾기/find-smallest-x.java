0b222306248a384bbfb994550817f4ea0df1ccb1
YM <109326287+youmean0427@users.noreply.github.com>
2026-02-24 21:24:19 +0900

    Title: 가장 작은 x 찾기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 260224/가장 작은 x 찾기/find-smallest-x.java
+++ 260224/가장 작은 x 찾기/find-smallest-x.java
@@ -10,7 +10,7 @@ public class Main {
             b[i] = sc.nextInt();
         }
         int ans = Integer.MAX_VALUE;
-        for (int i = a[0]; i <= b[0]; i++)
+        for (int i = 0; i <= b[0]; i++)
         {
             int num = i;
             int flag = 0;
