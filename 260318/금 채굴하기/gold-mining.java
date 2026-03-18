6527bc30409998dabde35e4c36bb39fc4b07dfee
YM <109326287+youmean0427@users.noreply.github.com>
2026-03-18 23:19:57 +0900

    Title: 금 채굴하기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260318/금 채굴하기/gold-mining.java
+++ 260318/금 채굴하기/gold-mining.java
@@ -25,22 +25,28 @@ public class Main {
     public static int gold(int[][] grid, int x, int y, int n, int m)
     {
         int ans = 0;
-        int k = n - 1;
         int maxCost = 0;
         int goldCnt = 0;
+        int costSum = grid[x][y];
         int[] dirX = {0, 1, 0, -1};
         int[] dirY = {1, 0, -1, 0};
         int[] edgeX = {0, 0, 0, 0};
         int[] edgeY = {0, 0, 0, 0};
         int[][] visited = new int[n][n];
-        int costSum = grid[x][y];
         visited[x][y] = 1;
 
+        // K=0
+        if (grid[x][y] == 1)
+        {
+            ans = 1;
+        }
+
+        // K=1
         for (int i = 0; i < 4; i++)
         {
             if (x+dirX[i] < 0 || x+dirX[i] >= n || y+dirY[i] < 0 || y+dirY[i] >= n)
             {
-                break;
+                continue;
             }
             else {
                 costSum += grid[x+dirX[i]][y+dirY[i]];
@@ -53,7 +59,9 @@ public class Main {
         {
             ans = costSum;
         }
-        for (int i = 2; i <= k; i++)
+
+        // K=2~
+        for (int i = 2; i <= n; i++)
         {
             
             for (int j = 0; j < 4; j++)
@@ -65,21 +73,18 @@ public class Main {
 
                     if (ex+dirX[l] >= 0 && ex+dirX[l] < n && ey+dirY[l] >= 0 && ey+dirY[l] < n)
                     {
-                        if (j == 0 && l == 2 || j == 3 && l == 1 || j == 2 && l == 0 || j == 1 && l == 3)
-                        { 
-                            continue;
-                        }
-                        else if (visited[ex+dirX[l]][ey+dirY[l]] == 0) 
+                        if (visited[ex+dirX[l]][ey+dirY[l]] == 0) 
                         {
                             visited[ex+dirX[l]][ey+dirY[l]] = 1;
                             costSum += grid[ex+dirX[l]][ey+dirY[l]];
-                            if (j == l)
-                            {
-                                edgeX[j] = ex+dirX[l];
-                                edgeY[j] = ey+dirY[l];
-                            }
                         }
                     }  
+
+                    if (j == l)
+                    {
+                            edgeX[j] = ex+dirX[l];
+                            edgeY[j] = ey+dirY[l];
+                        }
                 }            
             }
             if ((costSum * m) - (i * i + (i + 1) * (i + 1)) > 0)
