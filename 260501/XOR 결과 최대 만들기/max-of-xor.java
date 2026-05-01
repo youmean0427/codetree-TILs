5252a1d0d715712bc04c4579318d39fa045229d5
YM <109326287+youmean0427@users.noreply.github.com>
2026-05-01 20:42:15 +0900

    [Passed] XOR 결과 최대 만들기 (153ms, 10MB)

--- 260501/XOR 결과 최대 만들기/max-of-xor.java
+++ 260501/XOR 결과 최대 만들기/max-of-xor.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class Main {
-    public static List<Integer> list = new ArrayList<>();
     public static int n, m;
     public static int[] A;
     public static int ans;
@@ -13,43 +12,27 @@ public class Main {
         for (int i = 0; i < n; i++) {
             A[i] = sc.nextInt();
         }
-        back(0, 0);
+        back(0, 0, 0);
         System.out.print(ans);
 
     }
 
-    public static void back(int currIdx, int cnt)
+    public static void back(int currIdx, int cnt, int result)
     {
         if (currIdx >= n)
         {
             if (cnt == m)
             {
-                xor();
+                xor(result);
             }
             return;
         }
-        
-        list.add(A[currIdx]);
-        back(currIdx+1, cnt+1);
-        list.remove(list.size()-1);
-
-        back(currIdx+1, cnt);
+        back(currIdx+1, cnt+1, result ^ A[currIdx]);
+        back(currIdx+1, cnt, result);
     }
 
-    public static void xor()
+    public static void xor(int result)
     {
-        int num = 0;
-        // for (int i = 0; i < list.size(); i++)
-        // {
-        //     System.out.print(list.get(i));
-        // }
-        // System.out.println();
-
-        for (int i = 0; i < list.size(); i++)
-        {
-            num ^= list.get(i);
-        }
-
-        ans = Math.max(ans, num);
+        ans = Math.max(ans, result);
     }
 }
